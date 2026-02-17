954fc45100cfec80cf0da830a314be9b2c419865
MoizDev <moisessolis.gow@hotmail.com>
2026-02-16 20:23:35 -0500

    The main.ts file has been corrected

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+
+    provideRouter(
+      routes,
+      withComponentInputBinding(), 
+      withViewTransitions()        
+    ),
+
+    provideHttpClient(
+      withFetch()                  
+    ),
+
+  ]
+}).catch((err) => console.error(err));
