882c96f6c03fbf4cbfd0a3c2683a08ee609f7f89
MoizDev <moisessolis.gow@hotmail.com>
2026-02-16 21:12:00 -0500

    The arrangement is uploaded to the loading modes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http'
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-
+    
     provideRouter(
       routes,
       withComponentInputBinding(), 
